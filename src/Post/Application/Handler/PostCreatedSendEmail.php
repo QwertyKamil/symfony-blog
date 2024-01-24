b8f1c7a334ef43b7d05f3d77a9676c9f2e996a91
Kamil Zaworski <k.zaworski@advoxstudio.com>
2024-01-24 20:16:32 +0100

    add sending email after create post

---
+++ src/Post/Application/Handler/PostCreatedSendEmail.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Post\Application\Handler;
+
+use App\Post\Domain\Event\PostCreated;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Mime\Email;
+
+#[AsMessageHandler(
+    method: 'handle'
+)]
+class PostCreatedSendEmail
+{
+    public function __construct(
+        private readonly MailerInterface $mailer,
+        #[Autowire('%admin_email%')] private readonly string $adminEmail
+    ) {
+    }
+
+    public function handle(PostCreated $postCreated): void
+    {
+        $email = (new Email())
+            ->from($this->adminEmail)
+            ->to($postCreated->email)
+            ->subject(sprintf('New post %s', $postCreated->title))
+            ->text(sprintf('New post %s created', $postCreated->title));
+
+        $this->mailer->send($email);
+    }
+}
